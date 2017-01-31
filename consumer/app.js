491dc17b5dd3cc84b44f16c1320a8937e26cf72d
Matheus Fidelis <msfidelis@outlook.com>
2017-01-31 13:50:39 -0200

    Dockerizando o NodeJS

--- consumer/app.js
+++ consumer/app.js
@@ -1,23 +1,24 @@
 #!/usr/bin/env node
 var amqp = require('amqplib');
 
-amqp.connect('amqp://rabbitmq:rabbitmq@0.0.0.0')
-.then(function(conn) {
-  return conn.createChannel();
-})
-.then(function(ch) {
-  // Quantas mensagens o consumidor irá pegar por vez.  Sem isso, ele irá pegar todas
-  ch.prefetch(1);
+setTimeout(function() { //Espera o Rabbitmq subir
+  amqp.connect('amqp://rabbitmq:rabbitmq@rabbitmq')
+  .then(function(conn) {
+    return conn.createChannel();
+  })
+  .then(function(ch) {
+    // Quantas mensagens o consumidor irá pegar por vez.  Sem isso, ele irá pegar todas
+    ch.prefetch(1);
 
-  //Cria um listening que vai disparar o callbacl sempre que houver uma nova mensagem na fila
-  ch.consume('email', function(msg) {
-    setTimeout(function() {
-      console.log('E-mail para ser enviado: %s', msg.content.toString());
-      // Retorna o ACK para o servidor. Está tudo ok, tira da fila!
-      ch.ack(msg);
+    //Cria um listening que vai disparar o callbacl sempre que houver uma nova mensagem na fila
+    ch.consume('email', function(msg) {
+      setTimeout(function() {
+        console.log('E-mail para ser enviado: %s', msg.content.toString());
+        // Retorna o ACK para o servidor. Está tudo ok, tira da fila!
+        ch.ack(msg);
 
-      //Retorna um No ACK para o servidor. Deu erro, volta pra fila.
-      //ch.nack(msg)
-    }, 2000); // Timeout pra simular alguma coisa pesada sendo processada
+        //ch.nack(msg) Retorna um No ACK para o servidor.
+      }, 2000); // Timeout pra simular alguma coisa pesada sendo processada
+    });
   });
-});
+}, 10000);
--- sender/app.js
+++ sender/app.js
@@ -3,22 +3,24 @@
 var amqp = require('amqplib');
 var random = require('randomstring');
 
-amqp.connect('amqp://rabbitmq:rabbitmq@0.0.0.0')
-.then(function(conn) {
-  return conn.createChannel();
-})
-.then(function(ch) {
+//Aguarda o Rabbitmq subir
+setTimeout(function() {
+  amqp.connect('amqp://rabbitmq:rabbitmq@rabbitmq')
+  .then(function(conn) {
+    return conn.createChannel();
+  })
+  .then(function(ch) {
+    ch.assertQueue('email', {durable: false}); //Cria uma Queue Caso não exista
 
-  ch.assertQueue('email', {durable: false}); //Cria uma Queue Caso não exista
+    setInterval(function() {
+      var msg = {};
+      msg.to = "destinatario@teste.com";
+      msg.body = random.generate(10);
+      message = JSON.stringify(msg);
 
-  setInterval(function() {
-    var msg = {};
-    msg.to = "destinatario@teste.com";
-    msg.body = random.generate(10);
-    message = JSON.stringify(msg);
-    
-    //Envia uma mensagem para a Queue em forma de Buffer
-    ch.sendToQueue('email', Buffer(message));
-    console.log("Enviando mensagem: " + message);
-  }, 500);
-});
+      //Envia uma mensagem para a Queue em forma de Buffer
+      ch.sendToQueue('email', Buffer(message));
+      console.log("Enviando mensagem: " + message);
+    }, 500);
+  });
+}, 7000);
